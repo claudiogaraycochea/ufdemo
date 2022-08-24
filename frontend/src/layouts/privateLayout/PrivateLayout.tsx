b9daf3f60e2d283f90e630586d0de4f5aef8fe1c
gitrebaila <gitrebaila@gmail.com>
2022-08-24 09:38:20 +0200

    Dashboard structure

--- frontend/src/layouts/privateLayout/PrivateLayout.tsx
+++ frontend/src/layouts/privateLayout/PrivateLayout.tsx
@@ -1,13 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import { Container, Header } from 'uf-ui-pack';
+import './PrivateLayout.css';
+import SideMenu from '../../components/sideMenu/SideMenu';
 
 function PrivateLayout(props: any) {
-  return <div className="private-layout">
-    <div>Header</div>
-    <div className="dark">
-      {props.children}
+  return (
+    /*<div className="private-layout">
+      <Header></Header>
+      <Container>
+        {props.children}
+      </Container>
+    </div>*/
+    <div className="private-layout">  
+      <div className="content-side">
+        <SideMenu />
+        <Container>
+          {props.children}
+        </Container>
+      </div>
     </div>
-    <div>Footer</div>
-  </div>
+  )
 }
 
 export default PrivateLayout
